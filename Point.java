366d25b1d50f1aeddfbd19c02e34488fa3773935
David Newman <david.newman.dev1@gmail.com>
2024-01-09 17:18:33 -0800

    push lesson to github for reference

---
+++ Point.java
@@ -0,0 +1,5 @@
+//as per slide 13 in class, creating the Point object
+public class Point {
+    int x;
+    int y;
+}
---
+++ PointMain.java
@@ -0,0 +1,15 @@
+import java.awt.Point;
+public class PointMain {
+    public static void main(String[] args) {
+        Point p1 = new Point();
+        p1.x = 5;
+        p1.y = -2;
+
+        Point p2 = new Point();
+        p2.x = -4;
+        p2.y = 3;
+
+        System.out.println("The x-coord is " + p1.x);
+
+    }
+}
