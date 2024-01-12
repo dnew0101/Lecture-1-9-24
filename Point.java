0013996e1c360eb8c1f8aeb0999c207f8d4e9758
David Newman <david.newman.dev1@gmail.com>
2024-01-11 17:18:27 -0800

    better method

--- Point.java
+++ Point.java
@@ -2,12 +2,17 @@
 public class Point {
     int x;
     int y;
-    public void setLocation(int newX, int newY) {
-        x = newX;
-        y = newY;
-    }
+    // public void setLocation(int newX, int newY) {
+    //     x = newX;
+    //     y = newY;
+    // }
+    // public void translate(int dx, int dy) {
+    //     x += dx;
+    //     y += dy;
+    // }
+
+    //Alternatively, it could also be written this way
     public void translate(int dx, int dy) {
-        x += dx;
-        y += dy;
+        setLocation(x + dx, y + dy);
     }
 }
