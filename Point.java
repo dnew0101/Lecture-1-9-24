75319fcccb55d6af6bfabcadc6b949289e53d479
David Newman <david.newman.dev1@gmail.com>
2024-01-11 17:34:11 -0800

    distance formula

--- Point.java
+++ Point.java
@@ -2,16 +2,35 @@
 public class Point {
     int x;
     int y;
-    // public void setLocation(int newX, int newY) {
-    //     x = newX;
-    //     y = newY;
-    // }
-    // public void translate(int dx, int dy) {
+    public void setLocation(int newX, int newY) {
+        x = newX;
+        y = newY;
+    }
+
+    public double distance(Point other) {
+        int dx = x - other.x;
+        int dy = y - other.y;
+        return Math.sqrt(dx * dx + dy * dy);
+        }
+
+    // public double distanceFromOrigin() {
+        // return Math.sqrt(x * x + y * y);
+        // }
+
+
+        // alternative distanceFromOrigin that uses distance
+    public double distanceFromOrigin() {
+        Point origin = new Point();
+        return distance(origin);
+        }
+
+
+//  public void translate(int dx, int dy) {
     //     x += dx;
     //     y += dy;
     // }
 
-    //Alternatively, it could also be written this way
+    //Alternatively, translate() could also be written this way
     public void translate(int dx, int dy) {
         setLocation(x + dx, y + dy);
     }
