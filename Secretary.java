2d8832762a459d8e7976a016977cdcdaf352682d
David Newman <david.newman.dev1@gmail.com>
2024-01-25 16:03:06 -0800

    small update

---
+++ Secretary.java
@@ -0,0 +1,6 @@
+//extend keyword gives Secretary class inherited values by default
+public class Secretary extends Employee {
+    public void takeDiction() {
+        System.out.println(getVacationForm());
+    }
+}
